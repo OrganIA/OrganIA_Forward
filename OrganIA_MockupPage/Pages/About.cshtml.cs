5bdd8d70bee21cd7e05abfe8d416732387e8f895
Victor-Hamon <victor.hamon@epitech.eu>
2020-11-30 18:06:29 +0100

    [ADD] .cs files for pages

---
+++ OrganIA_MockupPage/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrganIA_MockupPage.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ OrganIA_MockupPage/Pages/ReceiverPanel.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OrganIA_MockupPage.Pages
+{
+    public class ReceiverPanel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

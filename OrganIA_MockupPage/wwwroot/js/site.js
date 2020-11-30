1c9624d9cb4c127150f9c53a2c447a9993381b38
Victor-Hamon <victor.hamon@epitech.eu>
2020-11-30 18:05:38 +0100

    [ADD] Pages and js in order to have a sidebar menu

--- OrganIA_MockupPage/wwwroot/js/site.js
+++ OrganIA_MockupPage/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar, #content').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+});

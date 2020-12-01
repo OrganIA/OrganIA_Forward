09115f487a36fc4e14e350b5c5f0c554d306ed86
Victor-Hamon <victor.hamon@epitech.eu>
2020-12-01 12:04:51 +0100

    JRA-1 #comment corrected indent issue

---
+++ OrganIA_MockupPage/Models/Donor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrganIA_MockupPage.Models
+{
+    public class Donor
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public DateTime DateOfArrival { get; set; }
+        [Required]
+        public string Organ { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Rhesus { get; set; }
+        [Required]
+        public char RhesusSign { get; set; }
+        [Required]
+        public string HospitalFrom { get; set; }
+        public string HospitalTo { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public int BMI { get; set; }
+        [Required]
+        public int BloodIonogramForeignKey { get; set; }
+        [Required]
+        public int DonorForeignKey { get; set; }
+    }
+}
